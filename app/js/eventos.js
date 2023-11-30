62448e36f976f45d3bf57bdf11e2faae7417c1dc
LenaEthan <142547625+LenaEthan@users.noreply.github.com>
2023-11-30 23:44:45 +0000

    Eventos

---
+++ app/js/eventos.js
@@ -0,0 +1,8 @@
+//console.log ("Funciona!!!");
+//let el = document.querySelector('#txtExemplo');
+//console.log(el);
+//el.addEventListener('focusout', function(e) {
+    //console.log(e.target.value);
+   // document.querySelector('#txtClone').value=e.target.value;});
+
+//console.log(el);
